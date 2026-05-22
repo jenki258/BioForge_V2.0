77024a1de2629b784cf1e873a4df7d180c460ff4
jenki258 <jackandnik45@gmail.com>
2026-05-22 21:29:32 +0300

    Command now can make up to 5k colonyRadis & 10k maxInfestedBlocks Now killing mobs on mat or infected block will give +10 resources to core Tried to fix the infection block sucking in

---
+++ src/main/java/net/jenkimods/bioforge/event/MatSlownessHandler.java
@@ -0,0 +1,25 @@
+package net.jenkimods.bioforge.event;
+
+import net.jenkimods.bioforge.block.MicrobialMatBlock;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = "bioforge", bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class MatSlownessHandler {
+
+    @SubscribeEvent
+    public static void onLivingTick(LivingEvent.LivingTickEvent event) {
+        LivingEntity entity = event.getEntity();
+        if (entity.level().isClientSide()) return;
+
+        BlockState state = entity.getBlockStateOn();
+        if (state.getBlock() instanceof MicrobialMatBlock) {
+            entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 20, 0, false, false, false));
+        }
+    }
+}
